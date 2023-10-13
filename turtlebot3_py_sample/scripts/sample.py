9287c07af4bf003495e4d3276ca020f56bd33dda
Mes <mes900903@gmail.com>
2023-10-14 05:35:06 +0800

    make the py sample same as cpp sample

--- turtlebot3_py_sample/scripts/sample.py
+++ turtlebot3_py_sample/scripts/sample.py
@@ -9,27 +9,25 @@ def RAD2DEG(r):
     return r * 180 / math.pi
 
 def callback(event):
+    global counter
+    
     counter += 1
-    rospy.loginfo("sample file called : %d\t times, data: ", counter, event.current_real)
+    rospy.loginfo("sample file called : %d times", counter)
     
 def scanCallback(scan):
     for i in range(360):
         degree = RAD2DEG(scan.angle_min + scan.angle_increment * i)
         if scan.ranges[i] > 0.001:
-            print("[LIDAR INFO]:angle-distance:[%4.1f, %5.3f]", i, scan.ranges[i])
+            print("[LIDAR INFO]:angle-distance:[{:>4.1f}, {:>5.3f}]".format(i, scan.ranges[i]))
 
 def listener():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('listener')
+    
+    timer1 = rospy.Timer(rospy.Duration(0.1), callback)
+    
+    sub = rospy.Subscriber("/scan", LaserScan, scanCallback)
 
-    rospy.Subscriber("chatter", String, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
